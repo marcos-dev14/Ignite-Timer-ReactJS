e5cd82ebe9405c88245bafdcea73d4da9113d8cf
marcos-dev14 <mprogramador.10@gmail.com>
2022-10-24 11:19:00 -0300

    feat: Configurando ESLint

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
-import { ThemeProvider } from "styled-components";
+import { ThemeProvider } from 'styled-components'
 
-import { defaultTheme } from "./styles/themes/default";
-import { GlobalStyle } from "./styles/themes/global";
+import { defaultTheme } from './styles/themes/default'
+import { GlobalStyle } from './styles/themes/global'
 
 export function App() {
   return (
